43b098193382ed5deae93ce9b154cf88e4a43629
Petr Sejvl <petr.sejvl@plus4u.net>
2020-06-20 12:37:22 +0200

    removing folder required for school purposes only

--- sejvlpet/examples/tests/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for filename in *.cpp; do
-	if test -f "$filename"; then
-		g++ ./$filename && ./a.out
-		rm ./a.out
-	fi
-done
--- sejvlpet/src/logger.cpp
+++
@@ -1,3 +0,0 @@
-#include "logger.h"
-
-Logger::Logger(std::string format) : _format(std::move(format)) {}
